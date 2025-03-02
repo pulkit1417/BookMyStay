c5998594cb26ee23dfda3044ae50d51673d74d7f
pulkit1417 <gupta.pulkit2408@gmail.com>
2025-03-02 14:48:59 +0530

    bug fixed

--- components/Helper/SectionHeading.tsx
+++ components/Helper/SectionHeading.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 type props = {
-    heading: String;
-    about:String;
+    heading: string;
+    about:string;
 }
 const SectionHeading = ({heading, about}:props) => {
   return (
